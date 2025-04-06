91ccd22ecb26f834cd2b0b1fee1f6d6ccd028202
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-07 00:23:01 +0200

    Initialize docker for MongoDB with demo data

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # publikacio
 Publikáció bíráló rendszer a Programrendszerek fejlesztése 2025 tárgy keretében
+
+# Futtatás
+
+## MongoDB
+
+`docker compose up -d`
+
+Az adatbázis az alábbi linken érhető el: http://localhost:8081/
+
+# Adatbázis
+
+A docker indítása után a `publication` adatbázis tartalmaz néhány kezdeti adatot.
+
+## Kollekciók
+
+### role
+
+- id
+- name: a felhasználószerep neve, ez lehet `Author`, `Editor` vagy `Reviewer`
+
+### user
+
+- id
+- email
+- password
+- firstName
+- lastName
+- role: a szerep id-ja, amibe a felhasználó tartozik
+
+Kezdtei felhasználók (jelszó nélkül):
+- author@mailinator.com
+- editor@mailinator.com
+- reviewer@mailinator.com
+
+### article
+
+- id
+- author: a cikk szerzőjének id-ja
+- title
+- content
+- reviewers: a cikk bírálóinak id-ja egy listában
+- isAccepted
+
+### review
+
+- id
+- text
+- article: a cikk id-ja, amihez az értékelés tartozik
+- reviewer: a bíráló id-ja, aki az értékelést készítette
---
+++ setup/mongo-init-scripts/init.js
@@ -0,0 +1,53 @@
+db.auth('root', 'example');
+
+db = db.getSiblingDB('publication');
+
+db.role.insertMany([
+	{ _id: '67f2ffa3768572e24b9f0efb', name: 'Author' },
+	{ _id: '67f2ffac768572e24b9f0efd', name: 'Reviewer' },
+	{ _id: '67f2ffb4768572e24b9f0eff', name: 'Editor' },
+]);
+
+db.article.insert({
+	_id: '67f2f12b4ff1117b9f38bc48',
+	author: '67f2f0a94ff1117b9f38bc44',
+	title: 'My article',
+	content: 'This is my very first article.',
+	reviewers: ['67f2ef4a4ff1117b9f38bc40'],
+	isAccepted: false,
+});
+
+db.review.insert({
+	_id: '67f2f0134ff1117b9f38bc42',
+	text: 'Very good',
+	isAccepted: true,
+	article: '67f2f12b4ff1117b9f38bc48',
+	reviewer: '67f2ef4a4ff1117b9f38bc40',
+});
+
+db.user.insertMany([
+	{
+		_id: '67f2ef4a4ff1117b9f38bc40',
+		email: 'reviewer@mailinator.com',
+		password: '',
+		firstName: 'Réka',
+		lastName: 'Reviewer',
+		role: '67f2ffac768572e24b9f0efd',
+	},
+	{
+		_id: '67f2f0a94ff1117b9f38bc44',
+		email: 'author@mailinator.com',
+		password: '',
+		firstName: 'Augusztus',
+		lastName: 'Author',
+		role: '67f2ffa3768572e24b9f0efb',
+	},
+	{
+		_id: '67f2f0dd4ff1117b9f38bc46',
+		email: 'editor@mailinator.com',
+		password: '',
+		firstName: 'Edit',
+		lastName: 'Editor',
+		role: '67f2ffb4768572e24b9f0eff',
+	},
+]);
