1824eab00ed416365bfd3feb7672659f182b142b
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-07 23:22:51 +0200

    Create base for server side

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { Request, Response } from 'express';
+
+const app = express();
+const port = 5000;
+const dbUrl = 'mongodb://localhost:27017/publication';
+
+mongoose
+	.connect(dbUrl)
+	.then(() => {
+		console.log('Succesfully connected');
+	})
+	.catch((error) => {
+		console.log(error);
+		return;
+	});
+
+app.get('/', (req: Request, res: Response) => {
+	res.status(200).send('Hello, World!');
+});
+
+app.listen(port, () => {
+	console.log('Server is listening on port ' + port.toString());
+});
