9fa8ff38e9a53f24f874dfae04195d802ca9b1e9
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-14 00:48:37 +0200

    Create article edit route

--- server/src/routes/article-routes.ts
+++ server/src/routes/article-routes.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 
-import { PublicUser } from '../models/User';
+import { PublicUser, User } from '../models/User';
 import { Role } from '../models/Role';
 import { Article } from '../models/Article';
 import { ObjectId } from 'mongoose';
@@ -26,7 +26,6 @@ export const configureArticleRoutes = (router: Router): Router => {
 			});
 
 			Role.findById(user.role).then((role) => {
-				console.log(role);
 				if (!role) {
 					res.status(500).send('Internal server error');
 				} else if (role.name !== 'Author') {
@@ -41,6 +40,57 @@ export const configureArticleRoutes = (router: Router): Router => {
 							console.log(error);
 							res.status(500).send('Internal server error');
 						});
+				}
+			});
+		} else {
+			res.status(500).send('User is not logged in.');
+		}
+	});
+
+	router.post('/:articleId', (req: Request, res: Response) => {
+		if (req.isAuthenticated()) {
+			const user = req.user as PublicUser;
+
+			Role.findById(user.role).then((role) => {
+				if (!role) {
+					res.status(500).send('Internal server error');
+				} else if (role.name === 'Reviewer') {
+					res.status(401).send('Unauthorized');
+				} else {
+					Article.findById(req.params.articleId).then((article) => {
+						if (!article) {
+							res.status(404).send('Not found');
+						} else {
+							if (role.name === 'Author') {
+								if (article.readyForReview) {
+									res.status(500).send('This article cannot be edited');
+									return;
+								} else {
+									article.title = req.body.title;
+									article.content = req.body.content;
+									article.readyForReview = req.body.readyForReview;
+								}
+							} else {
+								if (!article.readyForReview) {
+									res
+										.status(500)
+										.send('This article is not ready to be reviewed');
+									return;
+								} else {
+									article.reviewers = req.body.reviewers;
+								}
+							}
+							article
+								.save()
+								.then((data) => {
+									res.status(200).send(data);
+								})
+								.catch((error) => {
+									console.log(error);
+									res.status(500).send('Internal server error');
+								});
+						}
+					});
 				}
 			});
 		} else {
