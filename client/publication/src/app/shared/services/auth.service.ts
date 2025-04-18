77870ac212cf49cc25dcb66b999055a74ed383e3
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-19 00:33:20 +0200

    Create auth service

--- client/publication/src/app/shared/services/auth.service.ts
+++ client/publication/src/app/shared/services/auth.service.ts
@@ -1,9 +1,58 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  URL_PREFIX = 'http://localhost:5000/user/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) {
+    const body = new URLSearchParams();
+    body.set('email', email);
+    body.set('password', password);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    return this.http.post(this.URL_PREFIX + 'login', body, {
+      headers: headers,
+      withCredentials: true,
+    });
+  }
+
+  register(user: User) {
+    const body = new URLSearchParams();
+    body.set('email', user.email);
+    body.set('password', user.password);
+    body.set('firstName', user.firstName);
+    body.set('lastName', user.lastName);
+    body.set('role', user.role);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    return this.http.post(this.URL_PREFIX + 'signup', body, {
+      headers: headers,
+    });
+  }
+
+  logout() {
+    return this.http.post(
+      this.URL_PREFIX + 'logout',
+      {},
+      { withCredentials: true, responseType: 'text' }
+    );
+  }
+
+  checkAuth() {
+    return this.http.get<boolean>(this.URL_PREFIX + 'is-authenticated', {
+      withCredentials: true,
+    });
+  }
 }
