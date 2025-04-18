731f752b93fde4f5301c3a83aa93c7407c0bc89b
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-19 00:03:40 +0200

    Generate auth components & guard & services

--- client/publication/src/app/app.module.ts
+++ client/publication/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
