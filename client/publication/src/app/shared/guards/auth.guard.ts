b3c5bbebad0175313e51f09b6d36af0bdd7133b3
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-19 00:33:29 +0200

    Create auth guard

--- client/publication/src/app/shared/guards/auth.guard.ts
+++ client/publication/src/app/shared/guards/auth.guard.ts
@@ -1,5 +1,24 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { catchError, map, of } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+  return inject(AuthService)
+    .checkAuth()
+    .pipe(
+      map((isAuthenticated) => {
+        if (!isAuthenticated) {
+          router.navigateByUrl('/login');
+          return false;
+        }
+        return true;
+      }),
+      catchError((error) => {
+        console.log(error);
+        router.navigateByUrl('/login');
+        return of(false);
+      })
+    );
 };
