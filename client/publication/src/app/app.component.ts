6f1f6116326d3887dec66f0fddbe72e0cc348856
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-19 00:48:06 +0200

    Update angular to 19

--- client/publication/src/app/app.component.ts
+++ client/publication/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
+  imports: [RouterOutlet],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'publication';
---
+++ client/publication/src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./components/auth/login/login.component').then(
+            (c) => c.LoginComponent
+          ),
+      },
+      {
+        path: 'signup',
+        loadComponent: () =>
+          import('./components/auth/signup/signup.component').then(
+            (c) => c.SignupComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: 'login',
+      },
+    ],
+  },
+  { path: '**', redirectTo: '/auth/login' },
+];
