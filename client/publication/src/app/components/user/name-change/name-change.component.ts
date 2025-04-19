f51cbd077e858ed0f01cb06d4d9d819bdd5255b7
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-19 22:29:57 +0200

    Generate components for profile

---
+++ client/publication/src/app/components/user/name-change/name-change.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-change',
+  imports: [],
+  templateUrl: './name-change.component.html',
+  styleUrl: './name-change.component.scss'
+})
+export class NameChangeComponent {
+
+}
