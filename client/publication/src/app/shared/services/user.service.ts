dadc9b5acbba0209613057f5a618c658d5de7bf0
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-19 22:27:31 +0200

    Create user service

--- client/publication/src/app/shared/services/user.service.ts
+++ client/publication/src/app/shared/services/user.service.ts
@@ -1,10 +1,64 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  URL_PREFIX = 'http://localhost:5000/user/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  getUser() {
+    return this.http.get<User>(this.URL_PREFIX, {
+      withCredentials: true,
+    });
+  }
+
+  getUserById(userId: string) {
+    return this.http.get<User>(this.URL_PREFIX + userId, {
+      withCredentials: true,
+    });
+  }
+
+  getReviewers() {
+    return this.http.get<User[]>(this.URL_PREFIX + 'reviewers', {
+      withCredentials: true,
+    });
+  }
+
+  deleteUser() {
+    return this.http.delete(this.URL_PREFIX, { withCredentials: true });
+  }
+
+  updateUserName(firstName: string, lastName: string) {
+    const body = new URLSearchParams();
+    body.set('firstName', firstName);
+    body.set('lastName', lastName);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    return this.http.post(this.URL_PREFIX + 'update', body, {
+      headers: headers,
+      withCredentials: true,
+    });
+  }
+
+  updatePassword(oldPassword: string, newPassword: string) {
+    const body = new URLSearchParams();
+    body.set('password', oldPassword);
+    body.set('newPassword', newPassword);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    return this.http.post(this.URL_PREFIX + 'update-password', body, {
+      headers: headers,
+      withCredentials: true,
+    });
+  }
 }
