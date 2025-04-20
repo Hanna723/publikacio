0a198430eb8206b41b9077848042f8cf5fe4c934
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-20 02:45:49 +0200

    Create detail page for articles

--- client/publication/src/app/components/article/detail/detail.component.ts
+++ client/publication/src/app/components/article/detail/detail.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import { Router } from '@angular/router';
+
+import { Article } from 'src/app/shared/models/Article';
+import { ArticleService } from 'src/app/shared/services/article.service';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-detail',
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatChipsModule],
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss'
+  styleUrl: './detail.component.scss',
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  article?: Article;
+  authorName: string = '';
 
+  constructor(
+    private articleService: ArticleService,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const url = this.router.url.split('/');
+    const id = url[url.length - 1];
+
+    this.articleService.getById(id).subscribe(article => {
+      this.article = article;
+
+      this.userService.getUserById(article.author).subscribe(author => {
+        this.authorName = author.firstName + ' ' + author.lastName;
+        console.log(this.article)
+        console.log(this.authorName)
+      })
+    })
+  }
 }
