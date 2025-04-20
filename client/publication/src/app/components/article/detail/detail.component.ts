efa97c1530e4789e8be2cafec51100ddcc167fbb
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-04-20 11:42:37 +0200

    Add functionality to delete button

--- client/publication/src/app/components/article/detail/detail.component.ts
+++ client/publication/src/app/components/article/detail/detail.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
 
 import { Article } from 'src/app/shared/models/Article';
 import { User } from 'src/app/shared/models/User';
@@ -18,12 +20,14 @@ import { UserService } from 'src/app/shared/services/user.service';
     MatButtonModule,
     MatCardModule,
     MatChipsModule,
+    MatDialogModule,
     MatIconModule,
   ],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss',
 })
 export class DetailComponent implements OnInit {
+  readonly dialog = inject(MatDialog);
   article?: Article;
   authorName: string = '';
   user?: User;
@@ -48,6 +52,29 @@ export class DetailComponent implements OnInit {
 
     this.userService.getUser().subscribe((user) => {
       this.user = user;
+    });
+  }
+
+  editArticle() {}
+
+  deleteArticle() {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '350px',
+      data: {
+        title: 'Are you sure?',
+        submitButton: 'OK',
+        button: 'Cancel',
+      },
+    });
+
+    dialogRef.componentInstance.submitEvent.subscribe(() => {
+      if (!this.article?._id) {
+        return;
+      }
+
+      this.articleService.delete(this.article?._id).subscribe(() => {
+        this.router.navigateByUrl('/article/list');
+      });
     });
   }
 }
