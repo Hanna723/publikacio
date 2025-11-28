1537ec8ce07dcf3b04e0184189993e09838b525e
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2025-11-29 00:43:55 +0100

    Add metrics endpoint to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import express from 'express';
 import expressSession from 'express-session';
 import mongoose from 'mongoose';
 import passport from 'passport';
+import client from 'prom-client';
 
 import { configureArticleRoutes } from './routes/article-routes';
 import { configurePassport } from './passport/passport';
@@ -15,6 +16,38 @@ import { configureUserRoutes } from './routes/user-routes';
 const app = express();
 const port = 5000;
 const dbUrl = process.env.MONGO_URL;
+const register = new client.Registry();
+
+client.collectDefaultMetrics({ register });
+
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'path', 'status_code'],
+  buckets: [0.01, 0.05, 0.1, 0.5, 1]
+});
+
+const httpRequestTotal = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'path', 'status_code']
+});
+
+const calculationErrors = new client.Counter({
+  name: 'calculation_errors_total',
+  help: 'Total number of calculation errors',
+  labelNames: ['error_type']
+});
+
+const calculationTotal = new client.Counter({
+  name: 'calculations_total',
+  help: 'Total number of calculations performed',
+});
+
+register.registerMetric(httpRequestDuration);
+register.registerMetric(httpRequestTotal);
+register.registerMetric(calculationErrors);
+register.registerMetric(calculationTotal);
 
 if (dbUrl) {
 	mongoose
@@ -59,11 +92,24 @@ app.use(passport.session());
 
 configurePassport(passport);
 
+app.get('/metrics', async (_, res) => {
+	res.setHeader('Content-Type', register.contentType);
+
+	try {
+		const metrics = await register.metrics();
+		res.writeHead(200);
+		res.end(metrics);
+	} catch (err) {
+		res.writeHead(500);
+		res.end('Wrror collecting metrics');
+	}
+});
+
 app.use('/api/article', configureArticleRoutes(express.Router()));
 app.use('/api/review', configureReviewRoutes(express.Router()));
 app.use('/api/roles', configureRoleRoutes(express.Router()));
 app.use('/api/user', configureUserRoutes(passport, express.Router()));
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
 	console.log('Server is listening on port ' + port.toString());
 });
